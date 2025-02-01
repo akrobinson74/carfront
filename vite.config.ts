ec6155863b703a0338ea16a49749126116dd4f00
Adam Robinson <akrobinson74@gmail.com>
2025-02-01 17:56:28 -0500

    fixed vite test config issue; changed api url to ecs IP

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    // setupFiles: ['./src/testSetup.ts'],
-    globals: true,
-    environment: 'jsdom',
-  },
 })
